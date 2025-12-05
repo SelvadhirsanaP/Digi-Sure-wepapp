49b6123387a360b0e9477bba69df94d241af3ad8
SelvadhirsanaP <kit25cseaselvadhirsanap@gmail.com>
2025-12-05 19:14:07 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("DigiSure Backend Running");
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
